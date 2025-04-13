8a108055b1a833f7a39a0dc00c158a27dbf53cb9
martuwilson <williner.dev@gmail.com>
2025-04-13 12:31:20 -0300

    Ingreso y lectura de contacto + creacion archivo contacto.dat

--- agenda/main.cpp
+++ agenda/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <fstream>
+//#define DIR_ARCHIVO "contactos.dat" //OPCION 1
 using namespace std;
+
+const char DIR_ARCHIVO[] = "contactos.dat"; //OPCION 2
+const int MAX_CONTACTOS = 1000;
 
 struct Contacto {
     char nombre[30];
@@ -8,6 +13,31 @@ struct Contacto {
     char correo[45];
 };
 
+void ingresarContacto(Contacto c){
+    ofstream salida;
+    salida.open(DIR_ARCHIVO, ios::binary | ios::app); // Abre el archivo en modo binario y de append
+    // Se usa ios::app para agregar al final del archivo sin sobrescribir
+    if (!salida) {
+        cout << "Error al abrir el archivo." << endl;
+        return;
+    }
+    salida.write(reinterpret_cast<char*>(&c), sizeof(Contacto));
+    salida.close();
+}
+
+Contacto leerContacto(){
+    Contacto c;
+    cout<<"\t\tNombre: ";
+    cin.getline(c.nombre, 30);
+    cout<<"\t\tApellido: ";
+    cin.getline(c.apellido, 45);
+    cout<<"\t\tEdad: ";
+    cin>>c.edad;
+    cin.ignore(); // Limpiar el buffer de entrada , se usa post valor numerico
+    cout<<"\t\tCorreo: ";
+    cin.getline(c.correo, 45);
+    return c;
+}
 
 int main () {
  /* Agenda de contactos */
@@ -17,5 +47,8 @@ int main () {
  eliminar contacto,
  actualizar contacto,
  */
+
+ Contacto c = leerContacto();
+ ingresarContacto(c);
  return 0;
 }
