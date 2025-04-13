2dafd2e0d76d8d9d1174f608c8a51cb3cfe15fc7
martuwilson <williner.dev@gmail.com>
2025-04-13 13:47:35 -0300

    Buscar contacto + saber si existe + mostrar contacto encontrado

--- agenda/main.cpp
+++ agenda/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 //#define DIR_ARCHIVO "contactos.dat" //OPCION 1
 using namespace std;
 
@@ -39,16 +40,64 @@ Contacto leerContacto(){
     return c;
 }
 
-int main () {
- /* Agenda de contactos */
- /* Ingresar contacto,
- Buscar contacto,
- ver lista de contactos,
- eliminar contacto,
- actualizar contacto,
- */
+bool existeContacto(char nombre[], char apellido[]){
+    Contacto c;
+    ifstream entrada; // Se usa ifstream para leer el archivo
+    entrada.open(DIR_ARCHIVO, ios::binary); // Abre el archivo en modo binario
+    if (entrada.good()){
+        while (entrada.read((char*) &c, sizeof(Contacto))){
+            /* strcmp = string comparison */
+            if (strcmp(c.nombre, nombre) == 0 && strcmp(c.apellido, apellido) == 0){
+                // se usa 0 para comparar cadenas, si son iguales devuelve 0, si es menor devuelve -1 y si es mayor devuelve 1
+                entrada.close();
+                return true; // El contacto existe
+            }
+        }
+        entrada.close();
+        return false; // El contacto no existe
+    }else{
+        cout << "Error al abrir el archivo." << endl;
+        entrada.close();
+        return false;
+    }
+}
 
- Contacto c = leerContacto();
- ingresarContacto(c);
+Contacto buscarContacto(char nombre[], char apellido[]){
+    Contacto c;
+    ifstream entrada; 
+    entrada.open(DIR_ARCHIVO, ios::binary); 
+    if (entrada.good()){
+        while (entrada.read((char*) &c, sizeof(Contacto))){
+            if (strcmp(c.nombre, nombre) == 0 && strcmp(c.apellido, apellido) == 0){
+                entrada.close();
+                return c; 
+            }
+        }
+    }
+}
+
+void mostrarContacto(Contacto c){
+    cout << endl;
+    cout << "\t\t----------------------------------------" << endl;
+    cout << "\t\tContacto encontrado:" << endl;
+    cout << "\t\t----------------------------------------" << endl;
+    cout << "\t\tNombre: " << c.nombre << endl;
+    cout << "\t\tApellido: " << c.apellido << endl;
+    cout << "\t\tEdad: " << c.edad << endl;
+    cout << "\t\tCorreo: " << c.correo << endl;
+    cout << "\t\t----------------------------------------" << endl;
+    cout << endl;
+}
+
+
+int main () {
+ /* Contacto c = leerContacto();
+ ingresarContacto(c); */
+ if(existeContacto("Martin","Williner")){
+    Contacto c = buscarContacto("Martin","Williner");
+    mostrarContacto(c);
+ }else{
+    cout << "El contacto no existe." << endl;
+ }
  return 0;
 }
