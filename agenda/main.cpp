d3005461ed8569f263f5c973f24820623193d04b
martuwilson <williner.dev@gmail.com>
2025-04-13 12:11:07 -0300

    inicio agenda + estructura de datos contacto

---
+++ agenda/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+struct Contacto {
+    char nombre[30];
+    char apellido[45];
+    int edad;
+    char correo[45];
+};
+
+
+int main () {
+ /* Agenda de contactos */
+ /* Ingresar contacto,
+ Buscar contacto,
+ ver lista de contactos,
+ eliminar contacto,
+ actualizar contacto,
+ */
+ return 0;
+}
