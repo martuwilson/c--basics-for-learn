201c567c481c42a502c1195e8e9639fbc6a932a6
martuwilson <williner.dev@gmail.com>
2025-04-17 20:53:51 -0300

    borrar proyecto , iniciar de cero

--- sistema_reservas/src/usuario.cpp
+++
@@ -1,9 +0,0 @@
-#include "Usuario.h"
-#include <iostream>
-
-Usuario::Usuario(int id, std::string nombre, std::string email)
-    : id(id), nombre(nombre), email(email) {}
-
-void Usuario::mostrarPerfil() const {
-    std::cout << "Usuario #" << id << " - " << nombre << " (" << email << ")" << std::endl;
-}
